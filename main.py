930d1817d77d8532f60058866003a699039f17cc
Ashu Kher <ashukher1@gmail.com>
2020-08-07 20:41:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,21 @@ from pathlib import Path
 
 
 def create_diff(old_file, new_file):
+    """
+    Creates the diff and output it to the console 
+
+    :param old_file: Path for the old file 
+    :param new_file: Path for the new file
+
+    :return: None 
+    """
     file_1 = open(old_file).readlines()
     file_2 = open(new_file).readlines()
 
     delta = difflib.unified_diff(file_1, file_2, old_file.name, new_file.name)
     sys.stdout.writelines(delta)
+
+    Return None 
 
 
 def main():
@@ -23,6 +33,7 @@ def main():
     new_file = Path(args.new_file_version)
 
     create_diff(old_file, new_file)
+    return None 
 
 
 if __name__ == "__main__":
