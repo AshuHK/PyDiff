4d5bf52de8c9c287ba7020f7900ae06546968f10
Ashu Kher <ashukher1@gmail.com>
2020-08-07 20:20:23 -0400

    Inital start

---
+++ main.py
@@ -0,0 +1,30 @@
+import argparse
+import difflib
+import sys
+
+from pathlib import Path
+
+def create_diff(old_file, new_file): 
+  file_1 = open(old_file).readlines()
+  file_2 = open(new_file).readlines()
+
+  delta = difflib.unified_diff(file_1, file_2, old_file.name, new_file.name)
+  sys.stdout.writelines(delta) 
+
+def main():
+  print("""
+    To save diff to a file, run: 
+    python main.py file1 file2 > output_file
+  """)
+  parser = argparse.ArgumentParser() 
+  parser.add_argument("old_file_version")
+  parser.add_argument("new_file_version")
+  args = parser.parse_args()
+
+  old_file = Path(args.old_file_version)
+  new_file = Path(args.new_file_version)
+
+  create_diff(old_file, new_file) 
+
+if __name__ == "__main__":
+  main()
